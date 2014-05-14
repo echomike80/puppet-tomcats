a1c3abbb453525893fc8a32772067046bf0255f2
Marcel Emmert <marsL@posteo.de>
2014-05-14 06:41:50 +0200

    modified tomcat user

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,13 +3,13 @@ class tomcats {
   # system user for tomcat service
 	user { 'tomcat':
 		ensure => present,
-#		comment => 'ECG Tomcat Funktionsnutzer',
-#		home => '/home/tomcat',
-#		managehome => true,
-#		uid => '3002',
-#		gid => '100',
-#		shell => '/bin/bash',
-#		password => '$6$WTIzKbqC$dvQe9Bh.IxG0vVo/ILeMvHIuLg4eKd3FyhKHRxOCxuGGsr8WY.GkFh/oEdngC3HmN26.H7o334f/XLja6Hrf0.',
+		comment => 'ECG Tomcat Funktionsnutzer',
+		home => '/home/tomcat',
+		managehome => true,
+		uid => '3002',
+		gid => '100',
+		shell => '/bin/bash',
+		password => '$6$WTIzKbqC$dvQe9Bh.IxG0vVo/ILeMvHIuLg4eKd3FyhKHRxOCxuGGsr8WY.GkFh/oEdngC3HmN26.H7o334f/XLja6Hrf0.',
 	}
 
   # tomcat installation directory
