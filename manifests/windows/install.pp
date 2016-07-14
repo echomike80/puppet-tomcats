6a2f10a9451ec0f8ff81c9ee2e53f97e941cd0a2
Marcel Emmert <Marcel.Emmert@ecg-leipzig.de>
2016-07-14 15:35:27 +0200

    fixed windows tomcat xcopy_tomcat

--- manifests/windows/install.pp
+++ manifests/windows/install.pp
@@ -172,7 +172,7 @@ define tomcats::windows::install (
   # xcopy doc - http://www.microsoft.com/resources/documentation/windows/xp/all/proddocs/en-us/xcopy.mspx?mfr=true
   exec { "xcopy_tomcat_${inst_dir}":
 #    command => "cmd.exe /c xcopy ${install_tempdir_windows}\\apache-tomcat-${tomcat_release}\\*.* ${inst_dir}\\${pkg_tomcat}\\ /e /s /h",
-    command => "cmd.exe /c if exist ${inst_dir}\\${pkg_tomcat}\\lib del ${inst_dir}\\${pkg_tomcat}\\lib\\ecj*.jar & xcopy ${install_tempdir_windows}\\apache-tomcat-${tomcat_release}\\*.* ${inst_dir}\\${pkg_tomcat}\\ /e /s /h /Y",
+    command => "cmd.exe /c (if exist ${inst_dir}\\${pkg_tomcat}\\lib del ${inst_dir}\\${pkg_tomcat}\\lib\\ecj*.jar) & xcopy ${install_tempdir_windows}\\apache-tomcat-${tomcat_release}\\*.* ${inst_dir}\\${pkg_tomcat}\\ /e /s /h /Y",
 #    creates => "${inst_dir}\\${pkg_tomcat}\\bin",
     unless => "findstr ${tomcat_release} ${inst_dir}\\${pkg_tomcat}\\RELEASE-NOTES",
     require => Exec["clean_tomcat_source_webapps_${tomcat_number}"],
